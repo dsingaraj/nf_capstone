98d42aaf489454ee44cd1a4493672b19aa5d99d1
Dominic <dominic.singaraj.global@gmail.com>
2023-06-05 14:27:54 +0200

    added dev s3 bucket

--- main.tf
+++ main.tf
@@ -6,3 +6,11 @@ resource "aws_vpc" "myvpc" {
   cidr_block = "10.0.0.0/16"
 }
 
+resource "aws_s3_bucket" "example" {
+  bucket = "my-tf-test-bucket"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
