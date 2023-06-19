f19a34de45002d9e969edd37345b8e02b6c44060
Dominic <dominic.singaraj.global@gmail.com>
2023-06-19 15:03:28 +0200

    added tf code to add prod s3 bucket

--- main.tf
+++ main.tf
@@ -2,19 +2,6 @@ provider "aws" {
   region = "us-west-2"
 }
 
-resource "aws_vpc" "myvpc" {
-  cidr_block = "10.0.0.0/16"
-}
-
-resource "aws_s3_bucket" "example" {
-  bucket = "neufische-sample-test-bucket"
-
-  tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
-  }
-}
-
 resource "aws_s3_bucket" "dev" {
   bucket = "neufische-dev-bucket"
 
@@ -30,5 +17,14 @@ resource "aws_s3_bucket" "qa" {
   tags = {
     Name        = "My bucket"
     Environment = "Dev"
+  }
+}
+
+resource "aws_s3_bucket" "prod" {
+  bucket = "neufische-prod-bucket"
+
+  tags = {
+    Name        = "neufische-qa-bucket"
+    Environment = "QA"
   }
 }
