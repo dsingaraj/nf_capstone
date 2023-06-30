427eed31e47b812a20bb08c6179ea9823de29b9d
dsingaraj <68767026+dsingaraj@users.noreply.github.com>
2023-06-30 16:24:54 +0200

    updated security group

--- main.tf
+++ main.tf
@@ -61,7 +61,7 @@ resource "aws_instance" "deham6ec2"{
     ami = "ami-0b9ecf71fe947bbdd"
     instance_type = "t3.micro"
     key_name = "deham630062023"
-    vpc_security_group_ids = "sg-0668745a1e96b9e55"
+    vpc_security_group_ids = ["sg-0668745a1e96b9e55"]
     subnet_id = "subnet-05527afe89ba4d0ee"
 
     tags = {
