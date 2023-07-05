e587ec7ec6e1a8578fad68f5e5a587aa3782ad89
Dominic <dominic.singaraj.global@gmail.com>
2023-07-05 15:26:51 +0200

    deham6 demos

--- main.tf
+++ main.tf
@@ -68,3 +68,15 @@ resource "aws_instance" "deham6ec2"{
         Name = "deham6ec2"
     }
 }
+
+resource "aws_instance" "deham6demos"{
+    ami = "ami-0fa167c2af1ea0840"
+    instance_type = "t2.micro"
+    key_name = "deham630062023"
+    vpc_security_group_ids = ["sg-0668745a1e96b9e55"]
+    subnet_id = "subnet-05527afe89ba4d0ee"
+
+    tags = {
+        Name = "deham6demos"
+    }
+}
