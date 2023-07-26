5e9d83f7aa16a8c1449ee6040484a65c60c71f9c
Dominic Singaraj <dominic.singaraj.global@gmail.com>
2023-07-26 17:51:32 +0530

    User Story 2: Setup AWS infrastructure. Added Security group

---
+++ securityGroup.tf
@@ -0,0 +1,28 @@
+# Security Groups
+resource "aws_security_group" "nf_sg_http"{
+    vpc_id = aws_vpc.nf_vpc.id
+    name = "nf_securityGroup "
+    tags = {
+        Name = "nf_sg_http"
+    }     
+}
+
+# Ingress Security Port 80 (Inbound)
+resource "aws_security_group_rule" "nf_ingress_http"{
+    from_port = 80
+    protocol = "tcp"
+    security_group_id = aws_security_group.nf_sg_http.id
+    to_port= 80
+    type = "ingress"
+    cidr_blocks = ["0.0.0.0/0"] # <Specify the CIDR> instead of allowing it to public
+}
+
+# Ingress Security Port 443 (Inbound)
+resource "aws_security_group_rule" "nf_ingress_https"{
+    from_port = 443
+    protocol = "tcp"
+    security_group_id = aws_security_group.nf_sg_http.id
+    to_port= 443
+    type = "ingress"
+    cidr_blocks = ["0.0.0.0/0"] # <Specify the CIDR> instead of allowing it to public
+}
