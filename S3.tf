8dd3c2522a788557a6514b0bdf3edc2c79e0cf1e
Dominic Singaraj <dominic.singaraj.global@gmail.com>
2023-07-26 18:08:09 +0530

    fixed the s3 issue

--- S3.tf
+++ S3.tf
@@ -7,6 +7,6 @@ resource "aws_s3_bucket" "nf-s3-wordpress" {
 }
 
 resource "aws_s3_bucket_acl" "nf-s3-wordpress-acl" {
-  bucket = aws_s3_bucket.nf_s3_wordpress.id
+  bucket = aws_s3_bucket.wordpress.id
   acl    = "private"
 }
