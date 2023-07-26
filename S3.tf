ef528dbdcd91d0722a4a3602cc2b8f770c928836
Dominic Singaraj <dominic.singaraj.global@gmail.com>
2023-07-26 18:23:17 +0530

    Added S3 ownership to fix the access issue

--- S3.tf
+++ S3.tf
@@ -6,7 +6,15 @@ resource "aws_s3_bucket" "nf-s3-wordpress" {
   }
 }
 
+resource "aws_s3_bucket_ownership_controls" "s3-ownership" {
+  bucket = aws_s3_bucket.nf-s3-wordpress.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
 resource "aws_s3_bucket_acl" "nf-s3-wordpress-acl" {
+  depends_on = [aws_s3_bucket_ownership_controls.s3-ownership]
   bucket = aws_s3_bucket.nf-s3-wordpress.id
   acl    = "private"
 }
