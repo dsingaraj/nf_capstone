e5d9962e33a2f838152b5869ce073ccadbcb5cc8
Pascal Spielvogel <pascal-spielvogel@gmx.de>
2023-07-27 14:38:34 +0200

    added terraform version

--- main.tf
+++ main.tf
@@ -1,3 +1,11 @@
 provider "aws" {
   region = "us-west-2"
 }
+terraform {
+    required_providers {
+        aws = {
+            source  = "hashicorp/aws"
+            version = "~> 5.8.0"
+        }
+    }
+}
